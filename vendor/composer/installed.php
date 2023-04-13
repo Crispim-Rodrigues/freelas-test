fb95bd85d4465030249d16433c066835b8de61ff
unknown <crispim.rodrigues.oficial@gmail.com>
2023-04-13 12:01:43 -0300

    organização com MVC

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '64f3c2b7ab4973ea337cf77a164c6bfa8aa8534a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '64f3c2b7ab4973ea337cf77a164c6bfa8aa8534a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
