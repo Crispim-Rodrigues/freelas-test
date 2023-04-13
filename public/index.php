18127ac2a4c18fea7eca0ab2456a13c4258a4ccf
unknown <crispim.rodrigues.oficial@gmail.com>
2023-04-13 13:28:48 -0300

    logica da cruz pronta

--- public/index.php
+++ public/index.php
@@ -1,39 +1,62 @@
 <?php 
 
-    // cruz
-    $size = 9;
-    for ($i=0; $i < $size; $i++) { 
-        for ($j=0; $j < $size; $j++) { 
-            if ($i == (round($size * 0.30) - 1) || $j == (round($size * 0.5) - 1)) 
-            {
-                echo "x";
-            }
-            else 
-            {
-               echo " . "; 
-            }
-        }
-     echo "<br/>";
-    }
+require_once '../vendor/autoload.php';
 
-    echo "<br/>";
+use App\Controllers\MatrixController;
+
+$controller = new MatrixController();
+
+$size = isset($_GET['size']) ? $_GET['size'] : 5;
+
+$controller->generateMatrix($size);
+
+
+
+
+
+
+
+
+
+
+
+
+//   teste 1
+
+    // cruz
+    // $size = 9;
+    // for ($i=0; $i < $size; $i++) { 
+    //     for ($j=0; $j < $size; $j++) { 
+    //         if ($i == (round($size * 0.30) - 1) || $j == (round($size * 0.5) - 1)) 
+    //         {
+    //             echo "x";
+    //         }
+    //         else 
+    //         {
+    //            echo " . "; 
+    //         }
+    //     }
+    //  echo "<br/>";
+    // }
+
+    // echo "<br/>";
 
 
     //X
 
-    for ($i=0; $i < $size; $i++) { 
-        for ($j=0; $j < $size; $j++) { 
-            if ($i == $j || $i == ($size - $j - 1)) 
-            {
-               echo "x";
-            }
-            else
-            {
-                echo " . ";
-            }
-        }
-        echo "<br/>";
-    }
+    // for ($i=0; $i < $size; $i++) { 
+    //     for ($j=0; $j < $size; $j++) { 
+    //         if ($i == $j || $i == ($size - $j - 1)) 
+    //         {
+    //            echo "x";
+    //         }
+    //         else
+    //         {
+    //             echo " . ";
+    //         }
+    //     }
+    //     echo "<br/>";
+    // }
 
 
 
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit094188080ada0b6fc3bece8f46d12650
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit094188080ada0b6fc3bece8f46d12650', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit094188080ada0b6fc3bece8f46d12650', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit094188080ada0b6fc3bece8f46d12650
         call_user_func(\Composer\Autoload\ComposerStaticInit094188080ada0b6fc3bece8f46d12650::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit094188080ada0b6fc3bece8f46d12650::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
